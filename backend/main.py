956b85f23268da3d0b1f6f202de8b357df2e68d7
Gauri Singh <gaurisinghme1712@gmail.com>
2026-05-31 16:09:36 +0530

    updated cors

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "https://dogwalker.onrender.com"
+                   "https://zuppy.onrender.com"
                    ],  
     allow_credentials=True,
     allow_methods=["*"],  
